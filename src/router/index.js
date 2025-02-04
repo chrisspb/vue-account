a9e3b99258b776592df70c13f80a7940a2211f61
chrisspb <paghbirk.christian@gmail.com>
2025-02-04 10:50:19 +0100

    Update router configuration with security route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MonCompte from '../views/MonCompte.vue'
+import Security from '../views/Security.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,11 @@ const router = createRouter({
       path: '/mon-compte',
       name: 'mon-compte',
       component: MonCompte
+    },
+    {
+      path: '/security',
+      name: 'security',
+      component: Security
     }
   ]
 })
